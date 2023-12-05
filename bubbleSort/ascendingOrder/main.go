fb8634a9b04460df31d824106c19bb3dd5ad9b47
preyaswi <preyaswiprakashan@gmail.com>
2023-12-05 15:57:35 +0530

    optimized bubble sort

--- bubbleSort/ascendingOrder/main.go
+++ bubbleSort/ascendingOrder/main.go
@@ -11,11 +11,18 @@ func main() {
 }
 func AsecndingOrder(arr []int) {
 	n := len(arr)
+	var swap bool
 	for i := 0; i < n-1; i++ {
+		swap = false
 		for j := 0; j < n-i-1; j++ {
 			if arr[j] > arr[j+1] {
 				arr[j], arr[j+1] = arr[j+1], arr[j]
+				swap = true
 			}
+
+		}
+		if !swap {
+			break
 		}
 	}
 }
--- bubbleSort/sortStruct/main.go
+++ bubbleSort/sortStruct/main.go
@@ -23,10 +23,16 @@ func main() {
 func SortByName(students []Person) {
 	n := len(students)
 	for i := 0; i < n-1; i++ {
+var swap bool
 		for j := 0; j < n-i-1; j++ {
+			swap=false
 			if students[j].Name > students[j+1].Name {
 				students[j], students[j+1] = students[j+1], students[j]
+				swap=true
 			}
+		}
+		if !swap{
+			break
 		}
 	}
 }
