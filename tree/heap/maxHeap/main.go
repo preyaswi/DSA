b559310f122e1174e96a4963eca840004e9c7403
preyaswi <preyaswiprakashan@gmail.com>
2023-12-27 16:26:25 +0530

    maxheap updated

--- tree/heap/maxHeap/main.go
+++ tree/heap/maxHeap/main.go
@@ -2,69 +2,107 @@ package main
 
 import "fmt"
 
-type maxheap struct {
+type maxHeap struct {
 	arr []int
 }
 
-func main() {
-	arr := []int{11, 2, 7, 4,12, 5, 9,10}
-	h := &maxheap{}
-	h.buildHeap(arr)
-	fmt.Println("original array:")
-	display(h.arr)
-h.heapSort()
-fmt.Println("sorted array:")
-display(h.arr)
+func (h *maxHeap) insert(data int) {
+	h.arr = append(h.arr, data)
+	h.buildHeap()
+}
 
-}
-func(h *maxheap) buildHeap(arr []int)  {
-	h.arr=arr
-	for i:=parent(len(h.arr)-1);i>=0;i--{
-h.shiftdown(i)
+func (h *maxHeap) buildHeap() {
+	n := len(h.arr)
+	for i := n/2 - 1; i >= 0; i-- {
+		h.shiftDown(i)
 	}
 }
-func parent(i int)int  {
-	return (i-1)/2
-}
-func (h *maxheap)shiftdown(currentIdx int)  {
-	endIdx:=len(h.arr)-1
-	leftIdx:=leftchild(currentIdx)
-	for leftIdx<=endIdx{
-		var IdxToSwap int
-		rightIdx:=rightchild(currentIdx)
-		if rightIdx<=endIdx&&h.arr[rightIdx]>h.arr[leftIdx]{
-			IdxToSwap=rightIdx
-		}else {
-			IdxToSwap=leftIdx
+
+func (h *maxHeap) shiftDown(currentIdx int) {
+	endIdx := len(h.arr) - 1
+
+	for {
+		leftIdx := leftChild(currentIdx)
+		if leftIdx > endIdx {
+			break
 		}
-		if h.arr[IdxToSwap]>h.arr[currentIdx]{
-			swap(h.arr,IdxToSwap,currentIdx)
-			currentIdx=IdxToSwap
-			leftIdx=leftchild(currentIdx)
-		}else {
-			return
+
+		var idxToSwap int
+		rightIdx := rightChild(currentIdx)
+		if rightIdx <= endIdx && h.arr[rightIdx] > h.arr[leftIdx] {
+			idxToSwap = rightIdx
+		} else {
+			idxToSwap = leftIdx
+		}
+
+		if h.arr[idxToSwap] > h.arr[currentIdx] {
+			swap(h.arr, idxToSwap, currentIdx)
+			currentIdx = idxToSwap
+		} else {
+			break
 		}
 	}
 }
-func leftchild(i int)int  {
-	return 2*i+1
-}
-func rightchild(i int) int {
-	return 2*i+2
-}
-func swap(arr []int,a int,b int)  {
-	arr[a],arr[b]=arr[b],arr[a]
-}
-func display(arr []int)  {
-	for _,v:=range arr{
-		fmt.Print(v," ")
+
+func (h *maxHeap) delete() int {
+	if len(h.arr) == 0 {
+		return -1
 	}
-	fmt.Println()
+	maxValue := h.arr[0]
+	h.arr[0] = h.arr[len(h.arr)-1]
+	h.arr = h.arr[:len(h.arr)-1]
+	h.shiftDown(0)
+
+	return maxValue
 }
-func (h *maxheap)heapSort()[]int  {
-	for i:=len(h.arr)-1;i>=0;i--{
-		h.arr[0],h.arr[i]=h.arr[i],h.arr[0]
-		h.shiftdown(0)
+
+func (h *maxHeap) heapSort() []int {
+	n := len(h.arr)
+
+	for i := n - 1; i > 0; i-- {
+		h.arr[0], h.arr[i] = h.arr[i], h.arr[0]
+		h.shiftDown(0)
 	}
+
 	return h.arr
+}
+
+func leftChild(i int) int {
+	return 2*i + 1
+}
+
+func rightChild(i int) int {
+	return 2*i + 2
+}
+
+func parent(i int) int {
+	return (i - 1) / 2
+}
+
+func swap(arr []int, a int, b int) {
+	arr[a], arr[b] = arr[b], arr[a]
+}
+
+func display(arr []int) {
+	for _, v := range arr {
+		fmt.Print(v, " ")
+	}
+	fmt.Println(" ")
+}
+
+func main() {
+	var arr = []int{11, 2, 7, 4, 12, 5, 9, 10}
+	h := &maxHeap{}
+
+	for _, val := range arr {
+		h.insert(val)
+	}
+
+	fmt.Println("Original array:")
+	display(h.arr)
+
+	h.heapSort()
+
+	fmt.Println("Sorted array:")
+	display(h.arr)
 }
